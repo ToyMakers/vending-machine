a898cac8e55de18adb4fdb8978e7482be2fa23e7
OhSeungYoung <osy0056@khu.ac.kr>
2021-01-06 19:47:49 +0900

    feat: Set up components and redux

---
+++ client/src/pages/MainPage/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function index() {
+  return <h5>Miain</h5>;
+}
+
+export default index;
--- client/src/styles/theme.ts
+++ client/src/styles/theme.ts
@@ -1,17 +1,9 @@
-import { css } from 'styled-components';
-import { breakpoints } from '../constants/breakPoints';
+export const palette = {
+  main: '',
+};
 
-export const respondTo = Object.keys(breakpoints).reduce(
-  // [ 'mobile', 'tablet', 'desktop' ]
-  (accumulator: { [key: string]: any }, label: string) => {
-    accumulator[label as keyof typeof accumulator] = (
-      literals: TemplateStringsArray
-    ) => css`
-      @media (min-width: ${breakpoints[label]}) {
-        ${css(literals)};
-      }
-    `;
-    return accumulator;
-  },
-  {} // initial value
-);
+const theme = {
+  ...palette,
+};
+
+export default theme;
