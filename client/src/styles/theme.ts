7489562b1270a5c7459e04dfd87d12f3ebc50a75
OhSeungYoung <osy0056@khu.ac.kr>
2021-01-07 03:21:40 +0900

    feat: Add datas for cans

--- client/src/styles/theme.ts
+++ client/src/styles/theme.ts
@@ -1,8 +1,12 @@
 export const palette = {
-  vending_machine_inner: '#efefef',
   background: '#778ca3',
+  shelf_background: '#e9e3e3',
   coke_outer: '#7a2820',
   coke_inner: '#cb4335',
+  pepper_outer: '#561913',
+  pepper_inner: '#A93226',
+  sprite_outer: '#008B47',
+  sprite_inner: '#6BB64A',
 };
 
 const theme = {
