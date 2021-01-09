9c2e707cbe588ad2254302d41e2e67d4844dc33e
OhSeungYoung <osy0056@khu.ac.kr>
2021-01-10 07:28:23 +0900

    feat: Modify MainPage layout

--- client/src/pages/MainPage.tsx
+++ client/src/pages/MainPage.tsx
@@ -3,10 +3,28 @@ import styled from 'styled-components';
 import Footer from '../components/common/Footer';
 import Header from '../components/common/Header';
 import Machine from '../components/Machine';
+import Wallet from '../components/Wallet';
 
 const MainPageWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   height: 80vh;
   background-color: ${(props) => props.theme.background};
+`;
+
+const MachineArea = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const WalletArea = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 `;
 
 function MainPage() {
@@ -14,7 +32,13 @@ function MainPage() {
     <>
       <Header />
       <MainPageWrapper>
-        <Machine />
+        <MachineArea>
+          <Machine />
+        </MachineArea>
+        <div style={{ margin: '0rem 2rem' }} />
+        <WalletArea>
+          <Wallet />
+        </WalletArea>
       </MainPageWrapper>
       <Footer />
     </>
