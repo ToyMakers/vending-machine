b78f54c52be8c5194fd27913c35070b4cfe6ac05
OhSeungYoung <osy0056@khu.ac.kr>
2021-02-12 16:08:14 +0900

    feat: Add can data

--- client/src/constants/drinkData.ts
+++ client/src/constants/drinkData.ts
@@ -34,10 +34,58 @@ export const drinkData: SetDrinkType = {
     price: 1500,
     isFat: true,
   },
+  fanta: {
+    drinkName: 'fanta',
+    outerColor: palette.fantaOuter,
+    innerColor: palette.fantaInner,
+    price: 800,
+    isFat: false,
+  },
+  welchs: {
+    drinkName: 'welchs',
+    outerColor: palette.welchsOuter,
+    innerColor: palette.welchsInner,
+    price: 1300,
+    isFat: true,
+  },
+  welchsStrawberry: {
+    drinkName: 'welchs Strawberry',
+    outerColor: palette.welchsStrawberryOuter,
+    innerColor: palette.welchsStrawberryInner,
+    price: 1300,
+    isFat: true,
+  },
+  letsbe: {
+    drinkName: "let'sbe",
+    outerColor: palette.letsbeOuter,
+    innerColor: palette.letsbeInner,
+    price: 600,
+    isFat: false,
+  },
+  tejava: {
+    drinkName: 'tejava',
+    outerColor: palette.tejavaOuter,
+    innerColor: palette.tejavaInner,
+    price: 1000,
+    isFat: false,
+  },
+  pokari: {
+    drinkName: 'pokari',
+    outerColor: palette.pokariOuter,
+    innerColor: palette.pokariInner,
+    price: 1000,
+    isFat: false,
+  },
 };
 
 export const drinkStock = {
   coke: 10,
   sprite: 10,
   pepper: 1,
+  fanta: 5,
+  welchs: 3,
+  welchsStrawberry: 3,
+  letsbe: 5,
+  tejava: 5,
+  pokari: 5,
 };
--- client/src/styles/theme.ts
+++ client/src/styles/theme.ts
@@ -1,7 +1,7 @@
 export const palette = {
   main: '#a04ef6',
   point: '#5fd7f2',
-  shelfBackground: '#fbf8f8',
+  shelfBackground: '#f7f1e3',
   inventoryBackground: '#EEA47F',
   walletBackground: '#815854',
   cokeOuter: '#7a2820',
@@ -10,6 +10,18 @@ export const palette = {
   pepperInner: '#A93226',
   spriteOuter: '#008B47',
   spriteInner: '#6BB64A',
+  fantaOuter: '#c76701',
+  fantaInner: '#ff8403',
+  welchsOuter: '#700047',
+  welchsInner: '#B60775',
+  welchsStrawberryOuter: '#9c4179',
+  welchsStrawberryInner: '#d66faf',
+  letsbeOuter: '#003455',
+  letsbeInner: '#006AAD',
+  tejavaOuter: '#b1905c',
+  tejavaInner: '#E5BE82',
+  pokariOuter: '#0038FA',
+  pokariInner: '#1f50ff',
 };
 
 const theme = {
