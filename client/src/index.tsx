c2c077d297c737093c29cb45884d9ee82b4cabd8
OhSeungYoung <osy0056@khu.ac.kr>
2021-02-14 15:08:53 +0900

    feat: Add redux middleware to delay action

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './modules';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
+import { delayAction } from './middlewares/delayAction';
 
-const store = createStore(rootReducer, composeWithDevTools());
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(delayAction))
+);
 
 ReactDOM.render(
   <DndProvider backend={HTML5Backend}>
