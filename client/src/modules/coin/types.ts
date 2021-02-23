30464f4231deeeafac8db8f2ab12a42460ddc8ea
OhSeungYoung <osy0056@khu.ac.kr>
2021-02-23 19:54:47 +0900

    feat: Addd redux action to check whether the coin is dragging

--- client/src/modules/coin/types.ts
+++ client/src/modules/coin/types.ts
@@ -1,6 +1,7 @@
 import * as actions from './actions';
 
 export interface CoinState {
+  isCoinDragging: boolean;
   coinInMachine: number;
   coinInBox: number;
   coinInWallet: number;
@@ -9,6 +10,7 @@ export interface CoinState {
 // prepare types for all coin actions
 export type CoinAction =
   | ReturnType<typeof actions.initializeCoin>
+  | ReturnType<typeof actions.toggleCoinDragging>
   | ReturnType<typeof actions.pushCoin>
   | ReturnType<typeof actions.popCoin>
   | ReturnType<typeof actions.takeCoin>
